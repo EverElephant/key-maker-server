3380606b805197a518849fb9069f21832cad8893
727845268@qq.com <727845268@qq.com>
2020-01-18 20:58:41 +0800

    防止重复生成

--- src/main/java/com/everelp/keyMaker/mudules/controller/KeyController.java
+++ src/main/java/com/everelp/keyMaker/mudules/controller/KeyController.java
@@ -8,7 +8,6 @@ import com.everelp.keyMaker.common.util.UserUtil;
 import com.everelp.keyMaker.common.vo.ResCodeMsg;
 import com.everelp.keyMaker.common.vo.ServerRes;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,13 +33,14 @@ public class KeyController {
             serverRes.setCodeMsg(ResCodeMsg.NO_USER);
             return serverRes;
         }
-        makeKeys(user.getUserName(), json);
+        Date now = new Date();
+        makeKeys(user.getUserName(), json, now);
         MakerCache.nowUserName = user.getUserName();
-        MakerCache.makeTime = new Date().getTime();
+        MakerCache.makeTime = now.getTime();
         return serverRes;
     }
 
-    private boolean makeKeys(String userName, String json) {
+    private boolean makeKeys(String userName, String json, Date now) {
         Process p = RuntimeUtil.exec("make -C /home/ubuntu/nRF5_SDK_15.3.0/examples/ble_peripheral/ble_app_hids_keyboard/pca10040/s132/armgcc clean");
         while (true) {
             if (!p.isAlive()) {
@@ -65,13 +65,25 @@ public class KeyController {
                 break;
             }
         }
-        p = RuntimeUtil.exec("mkdir /home/ubuntu/nrf52/download/" + userName);
+        p = RuntimeUtil.exec("mkdir /home/ubuntu/nrf52/download/" + userName + now.getTime());
         while (true) {
             if (!p.isAlive()) {
                 break;
             }
         }
-        p = RuntimeUtil.exec("cp /home/ubuntu/nrf52/SDK15_app_s132.zip /home/ubuntu/nrf52/download/" + userName);
+        p = RuntimeUtil.exec("cp /home/ubuntu/nrf52/SDK15_app_s132.zip /home/ubuntu/nrf52/download/" + userName + now.getTime());
+        while (true) {
+            if (!p.isAlive()) {
+                break;
+            }
+        }
+        p = RuntimeUtil.exec("mkdir /var/www/html/download/" + userName);
+        while (true) {
+            if (!p.isAlive()) {
+                break;
+            }
+        }
+        p = RuntimeUtil.exec("cp /home/ubuntu/nrf52/download/" + userName + now.getTime()+"/SDK15_app_s132.zip /var/www/html/download/" + userName);
         while (true) {
             if (!p.isAlive()) {
                 break;
@@ -87,47 +99,21 @@ public class KeyController {
      * @param url      图片url
      * @param response 请求响应
      */
-    @RequestMapping(value = "/download")
-    public void getUrlFile(String url, HttpServletRequest request, HttpServletResponse response) {
-        // 这里的url，我为了测试，直接就写静态的。
-        User user = UserUtil.getCurrentUser(request);
-        if (user == null) {
-            response.setStatus(401);
-            return;
-        }
-        url = "/home/ubuntu/nrf52/download/" + user.getUserName() + "/SDK15_app_s132.zip";
-        File file = new File(url);
-
-        //判断文件是否存在如果不存在就返回默认图片或者进行异常处理
-        if (!(file.exists() && file.canRead())) {
-            response.setStatus(404);
-            return;
-        }
-        response.addHeader("Content-Disposition","attachment;filename=SDK15_app_s132.zip");
-        response.setContentType("application/x-zip-compressed;charset=utf-8");
-        try {
-            FileInputStream inputStream = new FileInputStream(file);
-            byte[] data = new byte[(int) file.length()];
-            inputStream.read(data);
-            OutputStream outputStream = response.getOutputStream();
-            outputStream.write(data);
-            outputStream.flush();
-            outputStream.close();
-            inputStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    @PostMapping(value = "/download")
+    public ServerRes getUrlFile(String url, HttpServletRequest request, HttpServletResponse response) {
+        ServerRes serverRes = new ServerRes(ResCodeMsg.SUCCESS);
+        serverRes.setData(request.getServerName()+"/download/"+UserUtil.getCurrentUser(request).getUserName()+"/SDK15_app_s132.zip");
+        return serverRes;
     }
 
     private boolean isMakeRunning() {
         long lastMakeTime = MakerCache.makeTime;
-        // 五分钟
-        if (new Date().getTime() - 300000 > lastMakeTime) {
+        // 超过五分钟一定可以再生成了
+        if (new Date().getTime() - 300000 < lastMakeTime) {
             return false;
         }
-        File file = new File("/home/ubuntu/nrf52/download/" + MakerCache.nowUserName + "/SDK15_app_s132.zip");
+        // 五分钟以内，看下对应的生成了没
+        File file = new File("/home/ubuntu/nrf52/download/" + MakerCache.nowUserName + MakerCache.makeTime + "/SDK15_app_s132.zip");
         if (file.exists() && file.canRead()) {
             return false;
         }
